06a33a63a839d7b4548f0306db58621c9a99dc78
is343 <anz17n@gmail.com>
2019-10-11 11:44:51 +0900

    add logout functionality

--- src/hooks/api/useApi.tsx
+++ src/hooks/api/useApi.tsx
@@ -6,9 +6,12 @@ export const useApi = () => {
   const rawApi = new Api()
   const [apiInitialized, setApiInitialized] = useState(false);
   const [api, setApi] = useState(rawApi)
-  const initApi = (keycloak: KeycloakInstance) => {
-    const apiResponse = api.setup(keycloak)
+
+  const initApi = (keycloak: KeycloakInstance, logout: () => void) => {
+    if (apiInitialized) setApiInitialized(false); // handle re-login
+    const apiResponse = api.setup(keycloak, logout)
     setApiInitialized(apiResponse);
   };
+
   return { api, apiInitialized, initApi }
 }
