3a100b9cba5d12175f2b70f2dedad92e638b76d5
is343 <anz17n@gmail.com>
2019-10-11 16:31:48 +0900

    add popup menu

---
+++ src/routes/shared/SvgIconWrapper.tsx
@@ -0,0 +1,19 @@
+import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon';
+import React from 'react';
+import { IconType } from 'react-icons';
+
+
+interface IconWrapperProps extends SvgIconProps {
+  children: IconType | React.ReactNode
+}
+/**
+ * Wraps svg icons from `react-icons` in a `SvgIcon` component from `material-ui`
+ * @param props the props that `material-ui` provides for their icons
+ */
+export const SvgIconWrapper = (props: IconWrapperProps) => {
+  return (
+    <SvgIcon {...props}>
+      {props.children}
+    </SvgIcon>
+  );
+}
