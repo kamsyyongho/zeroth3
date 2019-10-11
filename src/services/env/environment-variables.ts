e75abea44f53cd3f6d8e6a433dd68c8234787ef0
is343 <anz17n@gmail.com>
2019-10-11 13:44:27 +0900

    add env variables, edit tslint

--- src/services/env/environment-variables.ts
+++ src/services/env/environment-variables.ts
@@ -1,3 +1,8 @@
+/**
+ * @returns `true` if the `NODE_ENV === "production"`
+ */
+export const isProduction = process.env.NODE_ENV === 'production';
+export const NODE_ENV = process.env.NODE_ENV;
 export const KEYCLOAK_URL = process.env.REACT_APP_KEYCLOAK_URL;
 export const KEYCLOAK_REALM = process.env.REACT_APP_KEYCLOAK_REALM;
 export const KEYCLOAK_CLIENT_ID = process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
