c5625fa2a344c15e04a5a03b7889faa68491982d
is343 <anz17n@gmail.com>
2019-10-15 15:23:38 +0900

    export paths to constants

--- src/routes/shared/header/Header.tsx
+++ src/routes/shared/header/Header.tsx
@@ -2,8 +2,10 @@ import { Button, Toolbar } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useLocation } from "react-router-dom";
+import { I18nContext } from '../../../hooks/i18n/I18nContext';
+import { PATHS } from '../../../types';
 import MenuPopup from './components/MenuPopup';
 
 
@@ -18,55 +20,16 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-interface Path {
-  to: string
-  title: string
-}
-
-const PATHS: { [x: string]: Path } = {
-  home: {
-    to: "/",
-    title: "Home"
-  },
-  about: {
-    to: "/about",
-    title: "About"
-  },
-  topics: {
-    to: "/topics",
-    title: "Topics"
-  },
-  drawer: {
-    to: "/drawer",
-    title: "Drawer"
-  },
-  simple: {
-    to: "/simple",
-    title: "Simple"
-  },
-  multi: {
-    to: "/multi",
-    title: "Multi"
-  },
-  list: {
-    to: "/list",
-    title: "List"
-  },
-  table: {
-    to: "/table",
-    title: "Table"
-  },
-}
-
 const Header: React.FunctionComponent<{}> = (props) => {
   const classes = useStyles()
   const location = useLocation()
+  const { translate } = useContext(I18nContext);
 
   const pathButtons: JSX.Element[] = Object.keys(PATHS).map((key, index) => {
     const path = PATHS[key]
     const { to, title } = path
     const isCurrentPath = location.pathname === to
-    return (<Button key={index} component={Link} to={to} color={isCurrentPath ? "secondary" : "inherit"}>{title}</Button>)
+    return (<Button key={index} component={Link} to={to} color={isCurrentPath ? "secondary" : "inherit"}>{(translate(`path.${title}`))}</Button>)
   })
 
   return (
