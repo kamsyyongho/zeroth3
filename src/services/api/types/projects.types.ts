f6e3c7943d4ad87661aaa3a78b4a6726ea5fa75b
is343 <anz17n@gmail.com>
2019-10-29 18:31:23 +0900

    build model-config api

--- src/services/api/types/projects.types.ts
+++ src/services/api/types/projects.types.ts
@@ -1,4 +1,4 @@
-import { Project, Threshold } from '../../../types';
+import { ModelConfig, Project, Threshold } from '../../../types';
 import { GeneralApiProblem } from './api-problem.types';
 
 //////////////
@@ -31,7 +31,14 @@ export type getProjectsResult =
 export type postProjectResult =
   | { kind: 'ok'; project: Project }
   | GeneralApiProblem;
+export type deleteProjectResult = { kind: 'ok' } | GeneralApiProblem;
 export type updateProjectResult =
   | { kind: 'ok'; project: Project }
   | GeneralApiProblem;
-export type deleteProjectResult = { kind: 'ok' } | GeneralApiProblem;
+export type getModelConfigsResult =
+  | { kind: 'ok'; modelConfigs: ModelConfig[] }
+  | GeneralApiProblem;
+export type postModelConfigResult =
+  | { kind: 'ok'; modelConfig: ModelConfig }
+  | GeneralApiProblem;
+export type deleteModelConfigResult = { kind: 'ok' } | GeneralApiProblem;
