c9b7aca31e25914942bd202bbef0955d5481ab47
is343 <anz17n@gmail.com>
2019-10-31 13:50:23 +0900

    add methods for the organizations path

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 export * from './models.types';
+export * from './organizations.types';
 export * from './path.types';
 export * from './projects.types';
 export * from './snackbar.types';
 export * from './users.types';
+
