3aa4be78ab8f4ced56f114ea0b7337d78daf49da
is343 <anz17n@gmail.com>
2019-11-22 12:17:36 +0900

    add dropzone text i18n

--- src/routes/shared/form-fields/DropZoneFormField.tsx
+++ src/routes/shared/form-fields/DropZoneFormField.tsx
@@ -2,6 +2,7 @@ import { Box, FormControl, FormHelperText } from '@material-ui/core';
 import { FieldProps, getIn } from "formik";
 import { DropzoneArea, DropzoneAreaProps } from 'material-ui-dropzone';
 import React from "react";
+import { I18nContext } from '../../../hooks/i18n/I18nContext';
 
 /**
  * max upload file size in bytes
@@ -9,11 +10,29 @@ import React from "react";
  */
 const MAX_FILE_SIZE = 300000000;
 
+/**
+ * The size conversion that is used by `material-ui-dropzone`
+ * - this function has been copied VERBATIM (the types were added by me) to keep consistancy with their code
+ * @param filesize in bytes
+ */
+function convertBytesToMbsOrKbs(filesize: number) {
+  let size = '';
+  // I know, not technically correct...
+  if (filesize >= 1000000) {
+    size = (filesize / 1000000) + ' megabytes';
+  } else if (filesize >= 1000) {
+    size = (filesize / 1000) + ' kilobytes';
+  } else {
+    size = filesize + ' bytes';
+  }
+  return size;
+}
 
 interface DropZoneFormFieldProps extends FieldProps, DropzoneAreaProps {
   errorOverride?: boolean;
   errorTextOverride?: boolean;
   helperText?: string;
+  dropZoneText?: string;
   hidden?: boolean;
   fullWidth?: boolean;
 }
@@ -24,6 +43,7 @@ export const DropZoneFormField = ({
   errorOverride,
   errorTextOverride,
   helperText,
+  dropZoneText,
   filesLimit,
   maxFileSize,
   showPreviews,
@@ -36,9 +56,30 @@ export const DropZoneFormField = ({
   const errorText =
     getIn(form.touched, field.name) && getIn(form.errors, field.name);
   const isError = !!errorText || !!errorOverride;
+
+  const { translate } = React.useContext(I18nContext);
+
+  const handleRejectText = (
+    rejectedFile: { name: string; type: string | undefined; size: number; },
+    acceptedFiles: string[],
+    maxFileSize: number
+  ) => {
+    const main = translate('forms.dropZone.reject.main', { name: rejectedFile.name });
+    let message = main;
+    if (rejectedFile.type && !acceptedFiles.includes(rejectedFile.type)) {
+      const notSupported = translate('forms.dropZone.reject.notSupported');
+      message += `${notSupported} `;
+    }
+    if (rejectedFile.size > maxFileSize) {
+      const exceedSizeLimit = translate('forms.dropZone.reject.exceedSizeLimit', { size: convertBytesToMbsOrKbs(maxFileSize) });
+      message += `${exceedSizeLimit} `;
+    }
+    return message;
+  };
+
   return (
     <FormControl
-      style={{ display: hidden ? 'none' : undefined }}
+      style={{ display: hidden ? 'none' : undefined, minWidth: 300 }}
       fullWidth={fullWidth}
       error={isError}
     >
@@ -48,9 +89,15 @@ export const DropZoneFormField = ({
           acceptedFiles={acceptedFiles}
           maxFileSize={maxFileSize || MAX_FILE_SIZE}
           onChange={(selectedFiles: File[]) => form.setFieldValue(field.name, selectedFiles)}
+          dropzoneText={dropZoneText || translate('forms.dropZone.main')}
           showFileNamesInPreview={true}
           showPreviews={showPreviews}
           showPreviewsInDropzone={false}
+          useChipsForPreview={true}
+          getFileLimitExceedMessage={(filesLimit: number) => `File limit exeeded: ${filesLimit}`}
+          getFileAddedMessage={(fileName: string) => `File added: ${fileName}`}
+          getFileRemovedMessage={(fileName: string) => `File removed: ${fileName}`}
+          getDropRejectMessage={handleRejectText}
           {...props}
         />
       </Box>
