387abc3c31dcf0a55d251a450754e491bdd7838d
is343 <anz17n@gmail.com>
2019-12-11 15:12:40 +0900

    create search component

---
+++ src/routes/shared/SearchBar.tsx
@@ -0,0 +1,68 @@
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import SearchIcon from '@material-ui/icons/Search';
+import matchSorter from 'match-sorter';
+import React from 'react';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+  }),
+);
+
+
+interface SearchBarProps {
+  list: unknown[];
+  keys: string[];
+  onSearch: (filteredList: any[], searching: boolean) => void;
+}
+
+export const SearchBar = (props: SearchBarProps) => {
+  const { list, keys, onSearch } = props;
+  const [value, setValue] = React.useState('');
+
+  const classes = useStyles();
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown; }>) => {
+    const value = event.target.value as string;
+    setValue(value);
+    const results = matchSorter(list, value, { keys });
+    onSearch(results, !!value.length);
+  };
+
+  const clearSearch = () => {
+    setValue('');
+    onSearch(list, false);
+  };
+
+  return (
+    <TextField
+      className={classes.margin}
+      id="search-bar"
+      fullWidth
+      onChange={handleChange}
+      value={value}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon color='primary' />
+          </InputAdornment>
+        ),
+        endAdornment: (value.length ? <InputAdornment position="start">
+          <IconButton
+            color='secondary'
+            aria-label="clear-search"
+            onClick={clearSearch}
+          >
+            <HighlightOffIcon />
+          </IconButton>
+        </InputAdornment> : null)
+      }}
+    />
+  );
+};
