4b222fcfe92714c10ce4f7c601244e999cb5c0c2
is343 <anz17n@gmail.com>
2019-12-13 16:06:04 +0900

    update permission checks for drawer

--- src/constants/permission.constants.ts
+++ src/constants/permission.constants.ts
@@ -1,7 +1,7 @@
 import { ROLES } from '../types';
 
 export const PERMISSIONS = {
-  IAM: [ROLES.root, ROLES.admin],
+  users: [ROLES.root, ROLES.admin],
   organization: [ROLES.root, ROLES.admin],
   models: [ROLES.root, ROLES.manager],
   crud: [ROLES.root, ROLES.manager],
