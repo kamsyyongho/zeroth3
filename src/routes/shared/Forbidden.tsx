1cd95e8b7a8697855f78c87188c8a6cf5ab38138
is343 <anz17n@gmail.com>
2019-12-16 10:43:43 +0900

    add Forbidden component customization

--- src/routes/shared/Forbidden.tsx
+++ src/routes/shared/Forbidden.tsx
@@ -4,8 +4,19 @@ import { Link } from 'react-router-dom';
 import { I18nContext } from '../../hooks/i18n/I18nContext';
 import { PATHS } from '../../types/path.types';
 
-export function Forbidden() {
+interface ForbiddenProps {
+  text?: string;
+  link?: string;
+  CustomComponent?: React.ComponentType;
+}
+
+export function Forbidden(props: ForbiddenProps) {
+  const { text, link, CustomComponent } = props;
   const { translate } = React.useContext(I18nContext);
+
+  if (CustomComponent) {
+    return <CustomComponent />;
+  }
 
   return (<Grid
     container
@@ -16,10 +27,10 @@ export function Forbidden() {
     style={{ minHeight: '100vh' }}
   >
     <Grid item xs={3}>
-      <Typography variant='h4' >{'403 '}{translate('common.forbidden')}</Typography>
+      <Typography variant='h4' >{text || `403 ${translate('common.forbidden')}`}</Typography>
     </Grid>
     <Grid item xs={3}>
-      <Button color='primary' variant='contained' component={Link} to={PATHS.home.to || '/'} >{translate('path.home')}</Button>
+      <Button color='primary' variant='contained' component={Link} to={link ? link : (PATHS.home.to || '/')} >{translate('path.home')}</Button>
     </Grid>
   </Grid>);
 }
