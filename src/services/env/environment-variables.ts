06126261e52fea7e73431a54c5f97c37e24fe02a
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2020-04-24 16:30:56 +0900

    change build script and ENV variable usage

--- src/services/env/environment-variables.ts
+++ src/services/env/environment-variables.ts
@@ -2,8 +2,8 @@
  * @returns `true` if `NODE_ENV === "production"`
  */
 
-export const isProduction = process.env.NODE_ENV === 'production';
-export const NODE_ENV = process.env.NODE_ENV;
+export const isProduction = process.env.REACT_APP_ENV || process.env.NODE_ENV === 'production';
+export const NODE_ENV = process.env.REACT_APP_ENV || process.env.NODE_ENV;
 export const KEYCLOAK_URL = NODE_ENV === 'production'
     ? 'http://13.125.20.108:8080/auth'
     : 'http://13.125.20.108:9080/auth';
