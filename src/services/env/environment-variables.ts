65bb9b72700bfd2b1142beee514e8b1b1d317aa6
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2020-04-24 15:03:05 +0900

    change the environmental variable management from .env to environment-variable.ts

--- src/services/env/environment-variables.ts
+++ src/services/env/environment-variables.ts
@@ -1,13 +1,20 @@
 /**
  * @returns `true` if `NODE_ENV === "production"`
  */
+
 export const isProduction = process.env.NODE_ENV === 'production';
 export const NODE_ENV = process.env.NODE_ENV;
-export const KEYCLOAK_URL = process.env.REACT_APP_KEYCLOAK_URL;
-export const KEYCLOAK_REALM = process.env.REACT_APP_KEYCLOAK_REALM;
-export const KEYCLOAK_CLIENT_ID = process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const KEYCLOAK_URL = NODE_ENV === 'production'
+    ? 'http://13.125.20.108:8080/auth'
+    : 'http://13.125.20.108:9080/auth';
+export const KEYCLOAK_REALM = 'zeroth';
+export const KEYCLOAK_CLIENT_ID = 'zeroth-web';
+export const BACKEND_URL = NODE_ENV === 'production'
+    ? 'http://13.125.20.108:8081'
+    : 'http://13.125.20.108:9081';
 /**
  * Homepage to redirect to after logout
  */
-export const HOME_URL = process.env.REACT_APP_HOME_URL;
+export const HOME_URL = NODE_ENV === 'production'
+    ? 'http://staging.zeroth-ee.alabs.ai'
+    : 'http://test.zeroth-ee.alabs.ai';
