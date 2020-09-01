90c46daad5e60a56d66ccbd6b7bf30332d9230c6
Henry-Kim-Youngwoo <henry@atlaslabs.ai>
2020-09-01 13:33:08 +0900

    update dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -11,7 +11,7 @@ docker build -t $image_name:$version .
 ECS_REPO=161969600347.dkr.ecr.ap-northeast-2.amazonaws.com/zeroth/zeroth-ee-dashboard
 docker tag $image_name:$version $ECS_REPO:latest
 docker tag $image_name:$version $ECS_REPO:$version
-password=$(aws ecr get-login-password)
+password=$(aws ecr get-login-password --profile zeroth)
 docker login --username AWS --password $password 161969600347.dkr.ecr.ap-northeast-2.amazonaws.com
 docker push $ECS_REPO:latest
 docker push $ECS_REPO:$version
