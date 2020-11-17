583b163d9db33200698d1f729a78b60f9f6ff741
Brian Lee <brian@atlaslabs.ai>
2020-11-17 13:48:01 +0900

    revert changes

--- src/routes/model-config/ModelConfigListItemExpand.tsx
+++ src/routes/model-config/ModelConfigListItemExpand.tsx
@@ -136,8 +136,8 @@ export function ModelConfigListItemExpand(props: ModelConfigListItemExpandProps)
 
   const formSchema = yup.object({
     name: yup.string().min(VALIDATION.MODELS.ACOUSTIC.name.min, nameText).max(VALIDATION.MODELS.ACOUSTIC.name.max, nameText).required(requiredTranslationText).trim(),
-    selectedAcousticModelId: yup.string().when([], {is: () => !modelConfig.imported, then: yup.string().nullable().required(requiredTranslationText)}),
-    selectedLanguageModelId: yup.string().when([], {is: () => !modelConfig.imported,  then: yup.string().nullable().required(requiredTranslationText)}),
+    selectedAcousticModelId: yup.string().nullable().required(requiredTranslationText),
+    selectedLanguageModelId: yup.string().nullable().required(requiredTranslationText),
     thresholdLr: yup.number().typeError(numberText).min(VALIDATION.PROJECT.threshold.moreThan).nullable().test('lowRiskTest', translate('forms.validation.lessThan', { target: thresholdLrText, value: thresholdHrText }), function (thresholdLr) {
       const { thresholdHr } = this.parent;
       if (thresholdLr === 0 || thresholdHr === 0 || thresholdLr === null) return true;
